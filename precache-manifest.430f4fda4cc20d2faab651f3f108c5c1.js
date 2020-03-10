ee5d7a1941f90ae8afd0badd5be0cdfa6e646d4c
Dana Shacham <dshacham22@gmail.com>
2020-03-10 10:17:53 +0100

    Updates

---
+++ precache-manifest.430f4fda4cc20d2faab651f3f108c5c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1486ee6f34d2a145f2c4c2d58a0bff45",
+    "url": "./index.html"
+  },
+  {
+    "revision": "33e47865978d1e81b9ac",
+    "url": "./static/css/main.b1657efd.chunk.css"
+  },
+  {
+    "revision": "7bb3b3118b0f650fa947",
+    "url": "./static/js/2.9b51bb38.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.9b51bb38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33e47865978d1e81b9ac",
+    "url": "./static/js/main.06770a2e.chunk.js"
+  },
+  {
+    "revision": "7d2d16499b417a029b08",
+    "url": "./static/js/runtime-main.dc7f9edc.js"
+  }
+]);
